da320745e597e1c18425653e7a895eea822ed70d
Sebastian Urdanegui <sebasurdanegui@gmail.com>
2025-08-09 15:33:08 -0500

    chore: add hero-section and horizontal infinite scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,53 @@
+import InfiniteScroll from "@/components/infinite-scroll";
+
 export default function LandingPage() {
   return (
-    <div className="h-screen max-w-6xl w-full p-8">
-      <h1 className="text-4xl font-bold font-sora">Hello world</h1>
+    <div className="min-h-screen max-w-6xl w-full h-full p-8 flex flex-col gap-8">
+      <header
+        id="header-landing-page"
+        className="flex flex-row justify-between items-center"
+      >
+        <p className="font-sora text-2xl font-semibold">
+          suscripto
+        </p>
+        <button
+          className="font-geist text-lg cursor-pointer font-normal"
+        >
+          Iniciar sesión
+        </button>
+      </header>
+      <main
+        id="main-landing-page"
+        className="h-full bg-[url('/grid.svg')] bg-cover bg-center w-full flex flex-col justify-center items-center mask-fade-top-bottom px-4 py-8"
+      >
+        <section
+          id="hero-landing-page"
+          className="w-full flex flex-col justify-center items-center gap-8 p-2"
+        >
+          <h1
+            className="text-5xl font-bold font-sora text-center max-w-2xl"
+          >
+            Controla tus suscripciones en un solo lugar
+          </h1>
+          <p
+            className="font-geist font-normal text-lg text-center max-w-2xl"
+          >
+            Registra, organiza y recibe recordatorios antes de cada cobro. Conecta con Google Calendar y no vuelvas a pagar por algo que no usas.
+          </p>
+          <button
+            className="font-sora font-semibold text-base text-center w-fit px-4 py-3 cursor-pointer rounded-lg bg-white text-black"
+          >
+            Empezar gratis
+          </button>
+          <InfiniteScroll />
+        </section>
+      </main>
+      <footer
+        id="footer-landing-page"
+        className=""
+      >
+
+      </footer>
     </div>
   )
 }
---
+++ src/components/infinite-scroll.tsx
@@ -0,0 +1,30 @@
+import icons from "@/constants/icons";
+
+export default function InfiniteScroll() {
+  return (
+    <div className="overflow-hidden pt-12 py-4 px-4 flex flex-row gap-20 max-w-4xl group">
+      <ul className="min-w-full list-none shrink-0 flex flex-row gap-4 items-center justify-between animate-scroll group-hover:[animation-play-state:paused]">
+        {
+            icons.map((icon, index) => (
+              <li key={index}>
+                <div className="flex justify-center items-center opacity-50">
+                  {icon.icon}
+                </div>
+              </li>
+            ))
+        }
+      </ul>
+      <ul className="min-w-full list-none shrink-0 flex flex-row gap-4 items-center justify-between animate-scroll group-hover:[animation-play-state:paused]">
+        {
+            icons.map((icon, index) => (
+              <li key={index}>
+                <div className="flex justify-center items-center opacity-50">
+                  {icon.icon}
+                </div>
+              </li>
+            ))
+        }
+      </ul>
+    </div>
+  );
+}
