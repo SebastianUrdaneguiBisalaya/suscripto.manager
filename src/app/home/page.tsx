6eac6ddc1b2e8302ead73a431ee10ea2d069123a
Sebastian Urdanegui <sebasurdanegui@gmail.com>
2025-08-09 17:54:01 -0500

    chore: add user-profile component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,24 @@
+import UserProfile from "@/components/user-profile";
+
 export default function HomePage() {
   return (
-    <div>
+    <div
+      className="max-w-6xl w-full h-full flex flex-col gap-8 justify-start p-8"
+    >
+      <header
+        className="flex flex-row justify-between items-start"
+      >
+        <p className="font-sora text-lg sm:text-2xl font-semibold">
+          suscripto
+        </p>
+        <UserProfile />
+      </header>
 
+      <main
+        className=""
+      >
+
+      </main>
     </div>
   )
 }
