fa5686ea417f3a43a395329f6e7a2ed5e706c90a
Sebastian Urdanegui <sebasurdanegui@gmail.com>
2025-08-10 15:11:09 -0500

    chore: change the name of table route to subscription route

---
+++ src/app/home/subscriptions/page.tsx
@@ -0,0 +1,22 @@
+export default function SubscriptionsPage() {
+    return (
+        <div
+            className="flex flex-col gap-8 w-full h-full"
+        >
+            <div
+                className="flex flex-col gap-4 w-full"
+            >
+                <h1 className="font-sora text-2xl font-bold text-left text-white-cream">Suscripciones</h1>
+                <p
+                    className="font-geist text-gray-400"
+                >
+                    Visualiza el historial de pago de las suscripciones a lo largo del tiempo.
+                </p>
+            </div>
+            <div
+                className="flex flex-col gap-3 w-full"
+            >
+            </div>
+        </div>
+    )
+}
