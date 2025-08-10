8e56681d9087aeddcfd5f99247b4ad0709f64539
Sebastian Urdanegui <sebasurdanegui@gmail.com>
2025-08-10 16:18:48 -0500

    chore: add supabase config

---
+++ src/lib/supabase/middleware.ts
@@ -0,0 +1,44 @@
+
+import { createServerClient } from "@supabase/ssr";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function updateSession(request: NextRequest) {
+  const supabaseResponse = NextResponse.next({
+    request,
+  });
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) => {
+            request.cookies.set(name, value);
+          });
+          cookiesToSet.forEach(({ name, value, options }) => {
+            supabaseResponse.cookies.set(name, value, options);
+          });
+        },
+      },
+    }
+  );
+
+  try {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+      const url = request.nextUrl.clone();
+      url.pathname = "/login";
+      return NextResponse.redirect(url);
+    }
+  } catch (error) {
+    console.warn("Middleware error:", error);
+  }
+
+  return supabaseResponse;
+}
