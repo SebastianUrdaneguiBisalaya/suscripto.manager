f3ac1cc3ef468d138401ca66a628c9b44dadff87
Sebastian Urdanegui <sebasurdanegui@gmail.com>
2025-08-16 00:11:08 -0500

    chore: add toggle function to hidde sidebar when user clicks on the link

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -32,8 +32,11 @@ export default function Sidebar() {
             </button>
             {
                 isOpen && (
-                    <div className="fixed inset-0 backdrop-blur-lg z-[100] px-2 overflow-x-hidden">
-                        <div className="absolute top-0 right-0 w-full max-w-64 bg-black border-l border-gray-600 p-4 overflow-y-auto h-screen hidde-scrollbar flex flex-col items-end gap-8 overflow-x-hidden">
+                    <div
+                        className="fixed inset-0 backdrop-blur-lg z-[80] px-2 overflow-x-hidden"
+                        onClick={toggleSidebar}
+                    >
+                        <div className="absolute top-0 right-0 w-full max-w-64 bg-black border-l border-gray-600 p-4 overflow-y-auto h-screen hidde-scrollbar flex flex-col items-end gap-8 overflow-x-hidden z-[100]">
                             <button
                                 className="flex items-center justify-center bg-white-cream rounded-full p-1 cursor-pointer w-fit h-fit"
                                 type="button"
@@ -46,6 +49,7 @@ export default function Sidebar() {
                                 <Link
                                     href="/home/dashboard"
                                     className="flex flex-row items-center gap-2"
+                                    onClick={toggleSidebar}
                                 >
                                     <span>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#ffffff" d="M13 9V3h8v6zM3 13V3h8v10zm10 8V11h8v10zM3 21v-6h8v6z"/></svg>
@@ -57,6 +61,7 @@ export default function Sidebar() {
                                 <Link
                                     href="/home/subscriptions"
                                     className="flex flex-row items-center gap-2"
+                                    onClick={toggleSidebar}
                                 >
                                     <span>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#ffffff" d="M11 16H3v3q0 .825.588 1.413T5 21h6zm2 0v5h6q.825 0 1.413-.587T21 19v-3zm-2-2V9H3v5zm2 0h8V9h-8zM3 7h18V5q0-.825-.587-1.412T19 3H5q-.825 0-1.412.588T3 5z"/></svg>
@@ -68,6 +73,7 @@ export default function Sidebar() {
                                 <Link
                                     href="/home/notifications"
                                     className="flex flex-row items-center gap-2"
+                                    onClick={toggleSidebar}
                                 >
                                     <span>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="#ffffff" strokeWidth="2"><path strokeLinecap="round" strokeLinejoin="round" d="M6 19v-9a6 6 0 0 1 6-6v0a6 6 0 0 1 6 6v9M6 19h12M6 19H4m14 0h2m-9 3h2"/><circle cx="12" cy="3" r="1"/></g></svg>
